818be6861b4472be5ae8fb1580c136e1784376bc
Lynne Bones <lbones@il-b4.msk.21-school.ru>
2021-08-07 18:57:46 +0300

    ex01 geline done class inprogress

---
+++ day00/ex01/contact.hpp
@@ -0,0 +1,15 @@
+#ifndef CONTACT_HPP
+#define CONTACT_HPP
+#include<iostream>
+class contact
+{
+	public:
+		  void set_name();
+	privat:
+		  int index[8];
+		  string name[8];
+		  string
+		  
+
+};
+#endif
--- day00/ex01/main.cpp
+++ day00/ex01/main.cpp
@@ -1,22 +1,24 @@
 #include<iostream>
 int main()
 {
-	std:string	command;
-	
-	while(1)
+	std::string	command;
+	std::int index;
+	std::string
+	std::cout<<"PhoneBook:";
+	while(getline(std::cin,command))
 	{
-		std::cout<<"PhoneBook:";
-		std::cin>>command;
 		if(command=="ADD")
 		{
+			command_add()
 		}
-		else if(command=="SEARCH")
-		{
-		}
+			/*else if(command=="SEARCH")
+			{
+			}*/
 		else if(command=="EXIT")
 		{
-
+			std::cout<<"exit"<<std::endl;
+			break;
 		}
-
+		std::cout<<"PhoneBook:";	
 	}
 }
